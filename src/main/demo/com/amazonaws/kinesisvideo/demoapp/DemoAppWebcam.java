ae17934b8dea2ebd50c55e374ff70929dcd94547
Wilco van Duinkerken <wilco@sparkboxx.com>
2018-07-29 10:47:34 +0200

    also import CameraMediaSource

--- src/main/demo/com/amazonaws/kinesisvideo/demoapp/DemoAppWebcam.java
+++ src/main/demo/com/amazonaws/kinesisvideo/demoapp/DemoAppWebcam.java
@@ -11,6 +11,7 @@ import com.amazonaws.kinesisvideo.client.mediasource.MediaSource;
 import com.amazonaws.kinesisvideo.common.exception.KinesisVideoException;
 import com.amazonaws.kinesisvideo.demoapp.auth.AuthHelper;
 import com.amazonaws.kinesisvideo.java.client.KinesisVideoJavaClientFactory;
+import com.amazonaws.kinesisvideo.java.mediasource.camera.CameraMediaSource;
 import com.amazonaws.kinesisvideo.java.mediasource.file.ImageFileMediaSource;
 import com.amazonaws.kinesisvideo.java.mediasource.file.ImageFileMediaSourceConfiguration;
 import com.amazonaws.regions.Regions;
